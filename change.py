0a27b10b043b1261480012f33075df5b8865dbac
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-23 21:55:28 +0300

    get all data

--- change.py
+++ change.py
@@ -4,9 +4,16 @@ import json
 req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
 data = json.loads(req.text)
 
+date = ""
+valutes = []
+change = "JPY"
+res = {}
 for key, value in data.items():
+    if key == "Date":
+        date = value
     if type(value) ==  dict:
         for k, v in value.items():
-            print(k)
-            for i in v:
-                print(v[i])
+            valutes.append(k)
+            if k == "JPY":
+                res = v
+print(res["Name"])
