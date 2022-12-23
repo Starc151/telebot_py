cb626cdbb4ab1fb45719ea1fb2fc25c3a46bcf84
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-23 18:42:52 +0300

    print data

--- change.py
+++ change.py
@@ -2,4 +2,11 @@ import requests
 import json
 
 req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-json_string = req.text
+data = json.loads(req.text)
+
+for key, value in data.items():
+    if type(value) ==  dict:
+        for k, v in value.items():
+            print(k)
+            for i in v:
+                print(v[i])
