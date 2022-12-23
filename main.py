66b595c8817ad344536fb7ce407fcf772f87f503
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-23 16:20:57 +0300

    bot start

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+from tok import TG_TOK
+
+bot = telebot.TeleBot(TG_TOK)
+
+@bot.message_handler(commands=['start'])
+def start_bot(message):
+    bot.send_message(message.chat.id,'Привет!')
+
+bot.infinity_polling()
