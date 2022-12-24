0b3a13fb4a202ff3face8e4db7f6b29af8eea917
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-24 23:04:30 +0300

    change currency

--- change.py
+++ change.py
@@ -1,19 +1,22 @@
 import requests
 import json
-
-req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-data = json.loads(req.text)
-
-date = ""
-valutes = []
-change = "JPY"
-res = {}
-for key, value in data.items():
-    if key == "Date":
-        date = value
-    if type(value) ==  dict:
-        for k, v in value.items():
-            valutes.append(k)
-            if k == "JPY":
-                res = v
-print(res["Name"])
+def  select_currency(message):
+    valute = message.text
+    valute = str(valute).upper()
+    req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    data = json.loads(req.text)
+    valutes = []
+    res = {}
+    curhelp = []
+    for key, value in data.items():
+        if type(value) ==  dict:
+            for k, v in value.items():
+                curhelp.append(k+": "+str(v["Nominal"])+" "+v["Name"])
+                valutes.append(k)
+                if k == valute:
+                    res = v
+    if valute == "CURHELP":
+        return '\n'.join(curhelp)
+    if res == {}:
+        return 'Такой валюты нет.\nСписок можно посмотреть по команде "Curhelp"'
+    return f'{res["Nominal"]} {res["Name"]} стоит {res["Value"]} рублей'
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import telebot
+from change import select_currency
 from tok import TG_TOK
 bot = telebot.TeleBot(TG_TOK)
-
 @bot.message_handler(commands=['start'])
 def start_bot(message):
     bot.send_message(message.chat.id,'Привет!')
 
+@bot.message_handler(commands=['game'])
+def game_bot(message):
+    bot.send_message(message.chat.id,'Игра пока отключена')
+
+@bot.message_handler(commands=['change'])
+def change(message):
+    bot.send_message(message.chat.id,'Укажите кодировку нужной валюты')
+    bot.send_message(message.chat.id,'Список можно посмотреть по кноппе ниже')
+    bot.send_message(message.chat.id,'Curhelp')
+    res = bot.register_next_step_handler(message, select_currency_bot)
+
+def select_currency_bot(message):
+    bot.send_message(message.chat.id, select_currency(message))
 
 bot.infinity_polling()
