4124972ce36dedb87b10807aa215de6584acfaf7
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-26 14:54:06 +0300

    add button in process#!

--- change.py
+++ change.py
@@ -7,16 +7,53 @@ def  select_currency(message):
     data = json.loads(req.text)
     valutes = []
     res = {}
-    curhelp = []
+    # curhelp = []
     for key, value in data.items():
         if type(value) ==  dict:
             for k, v in value.items():
-                curhelp.append(k+": "+str(v["Nominal"])+" "+v["Name"])
+                # curhelp.append(k+": "+str(v["Nominal"])+" "+v["Name"])
                 valutes.append(k)
                 if k == valute:
                     res = v
-    if valute == "CURHELP":
-        return '\n'.join(curhelp)
+    # if valute == "CURHELP":
+    #     return '\n'.join(curhelp)
     if res == {}:
         return 'Такой валюты нет.\nСписок можно посмотреть по команде "Curhelp"'
     return f'{res["Nominal"]} {res["Name"]} стоит {res["Value"]} рублей'
+
+def dict_valutes():
+    dict_valutes = {'AUD': '1 Австралийский доллар',
+                    'AZN': '1 Азербайджанский манат',
+                    'GBP': '1 Фунт стерлингов Соединенного королевства',
+                    'AMD': '100 Армянских драмов',
+                    'BYN': '1 Белорусский рубль',
+                    'BGN': '1 Болгарский лев',
+                    'BRL': '1 Бразильский реал',
+                    'HUF': '100 Венгерских форинтов',
+                    'HKD': '10 Гонконгских долларов',
+                    'DKK': '10 Датских крон',
+                    'USD': '1 Доллар США',
+                    'EUR': '1 Евро',
+                    'INR': '100 Индийских рупий',
+                    'KZT': '100 Казахстанских тенге',
+                    'CAD': '1 Канадский доллар',
+                    'KGS': '100 Киргизских сомов',
+                    'CNY': '10 Китайских юаней',
+                    'MDL': '10 Молдавских леев',
+                    'NOK': '10 Норвежских крон',
+                    'PLN': '1 Польский злотый',
+                    'RON': '1 Румынский лей',
+                    'XDR': '1 СДР (специальные права заимствования)',
+                    'SGD': '1 Сингапурский доллар',
+                    'TJS': '10 Таджикских сомони',
+                    'TRY': '10 Турецких лир',
+                    'TMT': '1 Новый туркменский манат',
+                    'UZS': '10000 Узбекских сумов',
+                    'UAH': '10 Украинских гривен',
+                    'CZK': '10 Чешских крон',
+                    'SEK': '10 Шведских крон',
+                    'CHF': '1 Швейцарский франк',
+                    'ZAR': '10 Южноафриканских рэндов',
+                    'KRW': '1000 Вон Республики Корея',
+                    'JPY': '100 Японских иен'}
+    return dict_valutes
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+from telebot import types
 import telebot
-from change import select_currency
+from change import *
+
 from tok import TG_TOK
 bot = telebot.TeleBot(TG_TOK)
 @bot.message_handler(commands=['start'])
-def start_bot(message):
-    bot.send_message(message.chat.id,'Привет!')
+def start_bot(m):
+    bot.send_message(m.chat.id, 'Привет!')
 
 @bot.message_handler(commands=['game'])
-def game_bot(message):
-    bot.send_message(message.chat.id,'Игра пока отключена')
+def game_bot(m):
+    bot.send_message(m.chat.id, 'Игра пока отключена')
 
 @bot.message_handler(commands=['change'])
-def change(message):
-    bot.send_message(message.chat.id,'Укажите кодировку нужной валюты')
-    bot.send_message(message.chat.id,'Список можно посмотреть по кноппе ниже')
-    bot.send_message(message.chat.id,'Curhelp')
-    res = bot.register_next_step_handler(message, select_currency_bot)
+def change(m):
+    bot.send_message(m.chat.id, 'Ведите кодировку нужной валюты')
+    view_button_bot(m)
+    bot.register_next_step_handler(m, select_currency_bot)
+    bot.send_message(m.chat.id, dict_valutes(), reply_markup=types.ReplyKeyboardRemove(), parse_mode='Markdown')
 
-def select_currency_bot(message):
-    bot.send_message(message.chat.id, select_currency(message))
+def select_currency_bot(m):
+    bot.send_message(m.chat.id, select_currency(m))
+
+# @bot.message_handler(content_types=['text'])
+def view_button_bot(m):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кодировки валют")
+    markup.add(item1)
+    bot.send_message(m.chat.id,'Список можно посмотреть по кноппе ниже', reply_markup=markup)
+    # if m.text == "Кодировки валют":
+        # bot.send_message(m.chat.id, dict_valutes(), reply_markup=types.ReplyKeyboardRemove(), parse_mode='Markdown')
+
+    
 
 bot.infinity_polling()
